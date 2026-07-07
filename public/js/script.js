3a265fb4440eea9387f5ba35fae2bbe2a5b50d37
Raflyromeo <raflyromeo@users.noreply.github.com>
2026-07-07 23:05:15 +0700

    style: Menyesuaikan desain tampilan dan aset statis

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+// JavaScript Tambahan (Custom JS)
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Tambahkan script interaktif kustom Anda di sini
+    console.log("Halaman Login Inventaris Barang IT Loaded");
+});
